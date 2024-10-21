3785c5c9e2e221a369623f1170d0f967071ffacd
KhushbooBansiwal <bansiwalkhushboo8@gmail.com>
2024-10-21 16:41:49 +0530

    initial commit

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+ import {useState} from 'react';
+ 
+ const Heading = ({country}) => {
+    const [name, setName] = useState('khushi')
+    return (
+<div>
+    <h3>Hello,{name} from {country} </h3>
+    <button onClick={ () => {
+        setName('pooja')
+    }}>Click me</button>
+</div>
+    );
+ }
+  export default Heading
